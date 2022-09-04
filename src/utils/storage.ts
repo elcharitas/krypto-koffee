12eb39c1bb349f3f53f46f5695653c783d6563e6
elcharitas <jonathanirhodia@gmail.com>
2022-09-05 00:48:32 +0100

    feat: add storage utils

---
+++ src/utils/storage.ts
@@ -0,0 +1,33 @@
+/**
+ * @param text
+ * @returns
+ */
+export const parseJson = <T = unknown>(
+    text: string | null
+): Record<string, T> | null => {
+    try {
+        return JSON.parse(String(text));
+    } catch (e) {
+        return null;
+    }
+};
+
+export const { stringify } = JSON;
+
+export const Storage = {
+    getItem<T = unknown>(key: string, def: T) {
+        const item = this.store.getItem(key);
+        const storedValue = parseJson<T>(item);
+        return storedValue ?? def;
+    },
+    setItem(key: string, value: unknown) {
+        this.store.setItem(key, stringify(value));
+    },
+    get store() {
+        if (typeof window !== "undefined") return localStorage;
+        return {
+            getItem: () => null,
+            setItem: () => {},
+        };
+    },
+};
