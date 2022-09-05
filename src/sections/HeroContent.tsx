e40e074c06086f96b976b916cee03371b11d6abe
elcharitas <jonathanirhodia@gmail.com>
2022-09-05 12:09:41 +0100

    feat: carousel + extract content into sections

---
+++ src/sections/HeroContent.tsx
@@ -0,0 +1,60 @@
+import { FC } from "react";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { Content, Globe } from "src/components";
+
+export const HeroContent: FC = () => (
+    <Content sx={{ background: "transparent", py: 8 }}>
+        <Stack
+            spacing={2}
+            direction={{ xs: "column", md: "row" }}
+            alignItems="center"
+            justifyContent={{ xs: "center", md: "space-between" }}
+        >
+            <Box
+                sx={{
+                    textAlign: "center",
+                    "&:hover": {
+                        transform: "scale(1.2)",
+                        cursor: "pointer",
+                    },
+                }}
+            >
+                <Typography textTransform="uppercase">
+                    Welcome to PayMeMatic
+                </Typography>
+                <Typography
+                    variant="h1"
+                    sx={{
+                        fontWeight: "bold",
+                        fontFamily: "Poppins",
+                    }}
+                >
+                    Personalized{" "}
+                    <span style={{ color: "#ce93d8", font: "inherit" }}>
+                        Donations!
+                    </span>
+                </Typography>
+                <Typography sx={{ px: 4 }}>
+                    PayMeMatic is a growing means of personalized donations
+                    built for a decentralized and transparent world
+                </Typography>
+
+                <Button
+                    sx={{ textTransform: "capitalize", my: 2 }}
+                    color="secondary"
+                    variant="outlined"
+                >
+                    <Typography
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        Get Started
+                    </Typography>
+                </Button>
+            </Box>
+            <Globe width={580} height={580} />
+        </Stack>
+    </Content>
+);
