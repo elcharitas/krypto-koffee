b47a0acd2c5012f2609466aeb7c4f38db4b2480e
elcharitas <jonathanirhodia@gmail.com>
2022-09-05 03:01:15 +0100

    fix: resize globe by dimension

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -2,14 +2,12 @@ import { FC, useEffect, useRef } from "react";
 import createGlobe from "cobe";
 
 const GLOBE_OPTS = {
-    devicePixelRatio: 2,
-    width: 600 * 2,
-    height: 600 * 2,
     phi: 0,
     theta: 0,
     dark: 1,
     diffuse: 1.2,
     mapSamples: 16000,
+    devicePixelRatio: 2,
     mapBrightness: 6,
     markers: [],
 };
@@ -27,6 +25,8 @@ export const Globe: FC<IGlobe> = ({ width, height }) => {
 
         const globe = createGlobe(canvasRef.current, {
             ...GLOBE_OPTS,
+            width: width * 2,
+            height: height * 2,
             baseColor: [0.3, 0.3, 0.3],
             markerColor: [0.1, 0.8, 1],
             glowColor: [1, 1, 1],
