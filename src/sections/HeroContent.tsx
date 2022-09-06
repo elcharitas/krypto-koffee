b8d92b50458ebd893481aed7d619fc07cadf8e46
elcharitas <jonathanirhodia@gmail.com>
2022-09-06 07:45:05 +0100

    fix: increase description padding

--- src/sections/HeroContent.tsx
+++ src/sections/HeroContent.tsx
@@ -34,7 +34,7 @@ export const HeroContent: FC = () => (
                         Donations!
                     </span>
                 </Typography>
-                <Typography sx={{ px: 4 }}>
+                <Typography sx={{ px: 8 }}>
                     PayMeMatic is a growing means of personalized donations
                     built for a decentralized and transparent world
                 </Typography>
