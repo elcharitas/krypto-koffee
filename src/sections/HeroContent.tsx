c461c81a395bc1954616ded267ccf4bf1fbbac8b
elcharitas <jonathanirhodia@gmail.com>
2022-09-13 02:43:39 +0100

    feat: add page search form to hero

--- src/sections/HeroContent.tsx
+++ src/sections/HeroContent.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { Content, Globe } from "src/components";
 
 export const HeroContent: FC = () => (
@@ -10,7 +10,12 @@ export const HeroContent: FC = () => (
             alignItems="center"
             justifyContent={{ xs: "center", md: "space-between" }}
         >
-            <Box sx={{ textAlign: "center" }}>
+            <Stack
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{ textAlign: "center" }}
+            >
                 <Typography textTransform="uppercase">
                     Welcome to PayMeMatic
                 </Typography>
@@ -21,9 +26,9 @@ export const HeroContent: FC = () => (
                         fontFamily: "Poppins",
                     }}
                 >
-                    Personalized{" "}
+                    Membership{" "}
                     <span style={{ color: "#ce93d8", font: "inherit" }}>
-                        Donations!
+                        Subscription
                     </span>
                 </Typography>
                 <Typography sx={{ px: { xs: 2, md: 8 } }}>
@@ -31,21 +36,52 @@ export const HeroContent: FC = () => (
                     built for a decentralized and transparent world
                 </Typography>
 
-                <Button
-                    sx={{ textTransform: "capitalize", my: 2 }}
+                <TextField
                     color="secondary"
                     variant="outlined"
-                >
-                    <Typography
-                        sx={{
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        Get Started
-                    </Typography>
-                </Button>
-            </Box>
+                    placeholder="yourname"
+                    autoComplete="off"
+                    fullWidth
+                    InputProps={{
+                        autoFocus: true,
+                        sx: {
+                            pl: 3,
+                            borderRadius: "4em",
+                            maxWidth: "400px",
+                        },
+                        startAdornment: (
+                            <Typography fontWeight="bold">
+                                paymematic.app/
+                            </Typography>
+                        ),
+                        endAdornment: (
+                            <Button
+                                sx={{
+                                    textTransform: "capitalize",
+                                    borderRadius: "5em",
+                                    minWidth: "120px",
+                                    color: "white",
+                                }}
+                                color="secondary"
+                                variant="contained"
+                            >
+                                <Typography
+                                    sx={{
+                                        display: "flex",
+                                        alignItems: "right",
+                                    }}
+                                >
+                                    Claim PayPage
+                                </Typography>
+                            </Button>
+                        ),
+                    }}
+                    sx={{ m: 1, alignItems: "center" }}
+                />
+                <Typography>
+                    It&apos;s free to setup. All data is stored on blockchain
+                </Typography>
+            </Stack>
             <Box sx={{ display: { xs: "none", md: "flex" }, p: 8 }}>
                 <Globe width={560} height={560} />
             </Box>
