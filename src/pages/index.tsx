f18340b4e20a69b08c43c4f0d36c10188552b57e
elcharitas <jonathanirhodia@gmail.com>
2022-09-13 15:56:34 +0100

    feat: add dummy creators tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,64 @@
 import { FC } from "react";
-import { Carousel } from "src/components";
+import { GetServerSideProps } from "next";
+import { Tabs, Carousel, CreatorCard } from "src/components";
 import { HeroContent } from "src/sections";
+import { ECreatorCategory } from "src/types";
+import { denum } from "src/utils";
 
-const Page: FC = () => (
-    <>
-        <Carousel>
+const categories = denum(ECreatorCategory);
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const creators = [
+        {
+            address: "0x123",
+            category: ECreatorCategory.Art,
+        },
+        {
+            address: "0x456",
+            category: ECreatorCategory.Developer,
+        },
+    ];
+    return {
+        props: {
+            creators,
+        },
+    };
+};
+
+interface IHome {
+    creators: {
+        address: string;
+        category: ECreatorCategory;
+    }[];
+}
+const Page: FC<IHome> = ({ creators }) => {
+    return (
+        <>
             <HeroContent />
-        </Carousel>
-    </>
-);
+            <Tabs
+                color="secondary"
+                tabs={categories.map(([label, category]) => ({
+                    label,
+                    content: (
+                        <Carousel>
+                            {creators
+                                .filter((c) => c.category === category)
+                                .map((creator) => (
+                                    <CreatorCard
+                                        key={creator.address}
+                                        creator={{
+                                            ...creator,
+                                            photoURL: "/assets/avatar.jpeg",
+                                        }}
+                                    />
+                                ))}
+                        </Carousel>
+                    ),
+                }))}
+            />
+        </>
+    );
+};
 
 Page.displayName = "Home";
 
