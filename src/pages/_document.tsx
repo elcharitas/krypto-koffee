cf741ca3955912bb7d409def0f851ac41b9cfe2b
elcharitas <jonathanirhodia@gmail.com>
2022-09-13 15:31:45 +0100

    fix: disable scrollbar on body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,11 @@ const Document: FC = () => (
                 href="https://fonts.googleapis.com/css2?family=Nunito&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
                 rel="stylesheet"
             />
+            <style>
+                {`body::-webkit-scrollbar {
+                display: none;
+            }`}
+            </style>
         </Head>
         <body>
             <Main />
