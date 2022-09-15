8937b999d1f8d2f430253945c80125b2be6f3a4c
elcharitas <jonathanirhodia@gmail.com>
2022-09-15 10:58:46 +0100

    feat: create about creator section

---
+++ src/sections/AboutCreator.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import { Typography } from "@mui/material";
+import { Content } from "src/components";
+
+export const AboutCreator: FC = () => {
+    return <Content title="About Me">Well.</Content>;
+};
--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -2,3 +2,4 @@ export * from "./HeroContent";
 export * from "./AccountsModal";
 export * from "./HelpModal";
 export * from "./CreatorTab";
+export * from "./AboutCreator";
