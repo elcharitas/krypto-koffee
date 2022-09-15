b0cc7fc7973e70b48be31b0bd781547e386042ba
elcharitas <jonathanirhodia@gmail.com>
2022-09-15 05:21:15 +0100

    fix: support more config for carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,8 +2,12 @@ import { FC, ReactNode } from "react";
 import NukaCarousel from "nuka-carousel";
 
 interface ICarousel {
+    slidesToShow?: number;
+    slidesToScroll?: number;
+    slideIndex?: number;
+    speed?: number;
     children: ReactNode;
 }
-export const Carousel: FC<ICarousel> = ({ children }) => (
-    <NukaCarousel>{children}</NukaCarousel>
+export const Carousel: FC<ICarousel> = ({ children, ...props }) => (
+    <NukaCarousel {...props}>{children}</NukaCarousel>
 );
