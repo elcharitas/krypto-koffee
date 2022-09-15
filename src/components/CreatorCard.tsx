c589974dfc0ef081699acfdb744ec9ad2a63dcbe
elcharitas <jonathanirhodia@gmail.com>
2022-09-15 05:40:32 +0100

    feat: clickable creator cards

--- src/components/CreatorCard.tsx
+++ src/components/CreatorCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Image from "next/image";
-import { Typography } from "@mui/material";
+import NextLink from "next/link";
+import { Link, Typography } from "@mui/material";
 import { TCreator } from "src/types";
 import { Content } from "./Content";
 import { formatAddress } from "src/utils";
@@ -10,23 +11,31 @@ interface ICreatorCard {
 }
 export const CreatorCard: FC<ICreatorCard> = ({ creator }) => {
     return (
-        <Content sx={{ maxWidth: 170 }}>
-            {creator.photoURL && (
-                <Image src={creator.photoURL} width="150px" height="150px" />
-            )}
-            <Typography variant="body1" gutterBottom={!!creator.bio}>
-                {creator.name ? (
-                    <>
-                        <strong>{creator.name}</strong> (
-                        {formatAddress(creator.address)})
-                    </>
-                ) : (
-                    formatAddress(creator.address)
-                )}
-            </Typography>
-            {creator.bio && (
-                <Typography variant="body2">{creator.bio}</Typography>
-            )}
-        </Content>
+        <NextLink href={`/${creator.name}`} passHref>
+            <Link>
+                <Content sx={{ maxWidth: 170 }}>
+                    {creator.photoURL && (
+                        <Image
+                            src={creator.photoURL}
+                            width="150px"
+                            height="150px"
+                        />
+                    )}
+                    <Typography variant="body1" gutterBottom={!!creator.bio}>
+                        {creator.name ? (
+                            <>
+                                <strong>{creator.name}</strong> (
+                                {formatAddress(creator.address)})
+                            </>
+                        ) : (
+                            formatAddress(creator.address)
+                        )}
+                    </Typography>
+                    {creator.bio && (
+                        <Typography variant="body2">{creator.bio}</Typography>
+                    )}
+                </Content>
+            </Link>
+        </NextLink>
     );
 };
