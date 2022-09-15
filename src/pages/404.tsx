f0072412f552d42e17f7bae2d4f6b1ef9f135006
elcharitas <jonathanirhodia@gmail.com>
2022-09-15 11:12:15 +0100

    fix: update 404 error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,41 @@
 import { FC } from "react";
+import NextLink from "next/link";
+import { Button, Stack, Typography } from "@mui/material";
 import { IPage } from "src/types";
 
 type TError = IPage;
 const Page: FC<TError> = () => {
-    return <>🥲</>;
+    return (
+        <Stack
+            sx={{ minHeight: "90vh" }}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Typography
+                variant="h4"
+                fontWeight="bold"
+                fontFamily="Poppins"
+                gutterBottom
+            >
+                404 - No PayWall!
+            </Typography>
+            <Typography sx={{ my: 1 }}>
+                This paywall could still be available
+            </Typography>
+            <NextLink href="/" passHref>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    sx={{ color: "white" }}
+                >
+                    Claim PayWall
+                </Button>
+            </NextLink>
+        </Stack>
+    );
 };
+
+Page.displayName = "Not Found!";
+
 export default Page;
