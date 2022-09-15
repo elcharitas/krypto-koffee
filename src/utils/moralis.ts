e6f2745b10aba64da3be0b145047e19e9a3baccf
elcharitas <jonathanirhodia@gmail.com>
2022-09-15 16:11:49 +0100

    feat: export utils for balances

--- src/utils/moralis.ts
+++ src/utils/moralis.ts
@@ -3,5 +3,6 @@ import Moralis from "moralis";
 export const evmApi = Moralis.EvmApi;
 
 export const { getContractEvents } = evmApi.events;
+export const { getNativeBalance } = evmApi.balance;
 export const { getTokenPrice, getWalletTokenBalances } = evmApi.token;
 export const { resolveDomain } = evmApi.resolve;
